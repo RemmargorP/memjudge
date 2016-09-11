dfd681daebdff9f6a66c1081ca28abb095c4ffb5
Vadim Salavatov <xremmargorpx@gmail.com>
2016-09-11 23:19:40 +0300

    ...

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-echo "Building mjudgectl" && go build github.com/RemmargorP/mjudge/cmd/mjudgectl
-echo
-echo "Building createadmin" && go build github.com/RemmargorP/mjudge/cmd/createadmin
+mkdir -p bin > /dev/null 2> /dev/null
+cd bin && \
+echo "Building mjudgectl" && go build github.com/RemmargorP/mjudge/cmd/mjudgectl && \
+echo && \
+echo "Building createadmin" && go build github.com/RemmargorP/mjudge/cmd/createadmin && \
+cd ..
